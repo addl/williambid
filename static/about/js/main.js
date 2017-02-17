df4d876fe540d22ac912dfefb5bdc52d7e9cd5b8
Lion, Andry D <andrydaniel88@gmail.com>
2017-02-16 23:08:49 -0500

    Set up nginx configuration files

---
+++ static/about/js/main.js
@@ -0,0 +1,9 @@
+	$(function(){
+		$('#menu').slicknav();
+		$('.banner').addClass('animated bounceInDown');
+		$('.promo_area').addClass('animated bounceInLeft');
+		$('.banner_area p').addClass('animated bounceIn');
+		$('.satisfy').addClass('animated fadeInDown');
+		$('.wining').addClass('animated fadeInUp');
+		$('.bidday').addClass('animated flipInX');
+	});
